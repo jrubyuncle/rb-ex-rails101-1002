54ce1b35c699f2abc0aab0c6dffc90f970a348c1
jrubyuncle <jrubyuncle@gmail.com>
2015-10-02 17:08:43 +0800

    in routes.rb, add get 'welcome', to: 'topics#welcome'

--- first-app/config/routes.rb
+++ first-app/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :topics
+  get 'welcome', to: 'topics#welcome'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
