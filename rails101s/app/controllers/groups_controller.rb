570d9fbf481f363b5aa1df8a0c463b4f76556013
jrubyuncle <jrubyuncle@gmail.com>
2015-10-02 20:51:12 +0800

    3.6 add application helper: notice_message

--- rails101s/app/controllers/groups_controller.rb
+++ rails101s/app/controllers/groups_controller.rb
@@ -1,4 +1,7 @@
 class GroupsController < ApplicationController
   def index
+    flash[:notice] = "good morning"
+    #flash[:warning] = "this is warning"
+    #flash[:my_test] = "my_test"
   end
 end
