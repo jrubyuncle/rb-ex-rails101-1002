2c1b25a1e6ec55db3ffa639f74851f4320e1428d
jrubyuncle <jrubyuncle@gmail.com>
2015-10-06 19:39:10 +0800

    6.13 show edit/delete buttons only if author

--- rails101s/app/models/group.rb
+++ rails101s/app/models/group.rb
@@ -2,6 +2,11 @@ class Group < ActiveRecord::Base
   validates :title, presence: true
 
   has_many :posts
-  belongs_to :user
-  #belongs_to :owner, class_name: "User", foreign_key: :user_id
+  #belongs_to :user
+  belongs_to :owner, class_name: "User", foreign_key: :user_id
+
+  def editable_by?(user)
+    #return true
+    user && user == owner
+  end
 end
