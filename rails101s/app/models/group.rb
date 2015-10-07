e6a2421dcdd4900d5711ce325f290dae5792716e
jrubyuncle <jrubyuncle@gmail.com>
2015-10-07 12:18:50 +0800

    ch7.2-b +  1. set many-to-many association between user and group

--- rails101s/app/models/group.rb
+++ rails101s/app/models/group.rb
@@ -5,6 +5,8 @@ class Group < ActiveRecord::Base
   #belongs_to :user
   belongs_to :owner, class_name: "User", foreign_key: :user_id
 
+  has_many :group_users
+  has_many :members, through: :group_users, source: :user
   def editable_by?(user)
     #return true
     user && user == owner
