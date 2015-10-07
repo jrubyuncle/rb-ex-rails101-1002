e95f339b4fce680d3d3d52b089d863a1bc14d2b1
jrubyuncle <jrubyuncle@gmail.com>
2015-10-07 12:33:39 +0800

    7.3 only group members can create post

--- rails101s/app/controllers/posts_controller.rb
+++ rails101s/app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   before_action :find_group
+  before_action :member_required, only: [:new, :create]
 
   def new
     @post = @group.posts.new
@@ -46,5 +47,12 @@ class PostsController < ApplicationController
 
   def find_group
     @group = Group.find(params[:group_id])
+  end
+
+  def member_required
+    if ! current_user.is_member_of?(@group)
+      flash[:warning] = "you are not member so can not create post"
+      redirect_to group_path(@group)
+    end
   end
 end
