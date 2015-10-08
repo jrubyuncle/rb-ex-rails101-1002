a48d7c3e447b6f38b470ec724c2f4b237afd10f1
jrubyuncle <jrubyuncle@gmail.com>
2015-10-08 16:21:53 +0800

    fix typo

--- rails101s/app/models/group.rb
+++ rails101s/app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ActiveRecord::Base
   validates :title, presence: true
 
-  has_many :posts, dependant: :destroy
+  has_many :posts, dependent: :destroy
   #belongs_to :user
   belongs_to :owner, class_name: "User", foreign_key: :user_id
 
