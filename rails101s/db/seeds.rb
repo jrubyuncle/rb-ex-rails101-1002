7cc7d8b4f553b83fb452eee498f78130ee07c165
jrubyuncle <jrubyuncle@gmail.com>
2015-10-08 17:41:04 +0800

    ch10.3 write seeds.rb, rake db:seed, rake db:reset   1. rake db:reset        rake db:reset 是內建的 task ，會執行一整套流程, 包含           db:drop        ( 資料庫移除 )        => db:create      ( 資料庫建立 )        => db:schema:load ( 資料庫欄位建立 )        => db:seed        ( 執行資料庫種子 )

--- rails101s/db/seeds.rb
+++ rails101s/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "hello world"
+
+u = User.create(email: 'example@gmail.com', password: '12345678', password_confirmation: '12345678', name: 'test_account')
+
+for j in 1..20 do
+  Group.create(title: "group_no_#{j}",
+               description: "#{j}-th by seed",
+               user_id: u.id)
+
+  for k in 1..30 do
+    Post.create(group_id: j,
+      content: "post-#{k}-group-#{j}",
+      user_id: u.id)
+  end
+end
